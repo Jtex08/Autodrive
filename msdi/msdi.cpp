55e3da5a7f30ddd703ed4a1b9d0c8344a3199be3
jtex08 <jnoland08@tamu.edu>
2019-04-02 01:48:30 -0500

    MSDI Header

--- msdi/msdi.cpp
+++ msdi/msdi.cpp
@@ -41,3 +41,26 @@ int main(void)
   char info[11] = "Left Panel";
   left_msg.panel_location.data = info;
 
+
+  //Begin SPI setup
+
+   SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+    
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))
+    {
+    }
+
+    GPIOPinConfigure(GPIO_PA2_SSI0CLK);
+    GPIOPinConfigure(GPIO_PA3_SSI0FSS);
+    GPIOPinConfigure(GPIO_PA4_SSI0RX);
+    GPIOPinConfigure(GPIO_PA5_SSI0TX);
+
+    //If SSI0FSS fails to work switch to direct control
+    //GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE,GPIO_PIN_3);
+
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_3| GPIO_PIN_2);
