039e8349eba58d459cd097f3202ae426a7f8d8f6
jtex08 <jnoland08@tamu.edu>
2019-04-08 03:44:43 -0500

    Expanded driver

---
+++ msdi/msdi.c
@@ -0,0 +1,23 @@
+/*Texas A&M University
+**Electronic Systems Engineering Technology
+**ESET 420 Capstone II
+**Author: Jonathan Noland
+**File: msdi.c
+**------------------------------------------------------------------------------
+**This file contains the functions to use the TIC-10024-Q1 
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/gpio.h"
+#include "driverlib/ssi.h"
+#include "msdi.h"
+
+
--- msdi/msdi.h
+++ msdi/msdi.h
@@ -1,8 +1,15 @@
-//Defines and Macros for MSDI
-
+/*Texas A&M University
+**Electronic Systems Engineering Technology
+**ESET 420 Capstone II
+**Author: Jonathan Noland
+**File: msdi.h
+**------------------------------------------------------------------------------
+**This file contains the definitions and includes neccessary to use the TIC-10024-Q1 
+*/
 
 #ifndef __MSDI_H__
 #define __MSDI_H__
+
 
 //*****************************************************************************
 //
@@ -15,28 +22,42 @@ extern "C"
 {
 #endif
 
+/*
+* Error codes.
+*/
+typedef enum
+{
+    MSDI_STATUS_SUCCESS          = 0U,    /*!< No error. */
+    MSDI_STATUS_SPI_INIT         = 1U,    /*!< SPI initialization failure. */
+    MSDI_STATUS_COMM_ERROR       = 2U,    /*!< Error in communication. */
+} msdi_status_t;
+
+
+
 //*****************************************************************************
 //
 // The following values define the registers for the MSDI
 //
 //*****************************************************************************
-#define DEVICE_ID               0x01000000  // Device ID Register
-#define INT_STAT                0x02000000  // Interrupt Status Register
-#define CRC                     0x03000000  // CRC Result Register
-#define IN_STAT_MISC            0x04000000  // Misc Status Register
-#define IN_STAT_COMP            0x05000000  // Comparator Status Register
-#define CONFIG                  0x1A000000  // Device Global Configuration Register
-#define IN_EN                   0x1B000000  // Input Enable Register
-#define CS_SELECT               0x1C000000  // Current Source/Sink Selection Register
-#define WC_CFG0                 0x1D000000  // Wetting Current Configuration Register
-#define WC_CFG1                 0x1E000000  // Wetting Current Configuration Register
-#define CCP_CFG0                0x1F000000  // Clean Current Polling Register
-#define CCP_CFG1                0x20000000  // Clean Current Polling Register
-#define THRES_COMP              0x21000000  // Comparator Threshold Control Register
-#define INT_EN_COMP1            0x22000000  // Comparator Input Interrupt Generation Control Register
-#define INT_EN_COMP2            0x23000000  // Comparator Input Interrupt Generation Control Register
-#define INT_EN_CFG0             0x24000000  // Global Interrupt Generation Control Register
-
+typedef enum
+{
+    DEVICE_ID               =0x01000000U,  // Device ID Register
+    INT_STAT                =0x02000000U,  // Interrupt Status Register
+    CRC                     =0x03000000U,  // CRC Result Register
+    IN_STAT_MISC            =0x04000000U,  // Misc Status Register
+    IN_STAT_COMP            =0x05000000U,  // Comparator Status Register
+    CONFIG                  =0x1A000000U,  // Device Global Configuration Register
+    IN_EN                   =0x1B000000U,  // Input Enable Register
+    CS_SELECT               =0x1C000000U,  // Current Source/Sink Selection Register
+    WC_CFG0                 =0x1D000000U,  // Wetting Current Configuration Register
+    WC_CFG1                 =0x1E000000U,  // Wetting Current Configuration Register
+    CCP_CFG0                =0x1F000000U,  // Clean Current Polling Register
+    CCP_CFG1                =0x20000000U,  // Clean Current Polling Register
+    THRES_COMP              =0x21000000U,  // Comparator Threshold Control Register
+    INT_EN_COMP1            =0x22000000U,  // Comparator Input Interrupt Generation Control Register
+    INT_EN_COMP2            =0x23000000U,  // Comparator Input Interrupt Generation Control Register
+    INT_EN_CFG0             =0x24000000U  // Global Interrupt Generation Control Register
+} msdi_register_t;
 //*****************************************************************************
 //
 // The following values define the Read/Write Masks
@@ -53,6 +74,18 @@ extern "C"
 #define NUM_SSI_DATA            2
 
 
+/*******************************************************************************
+ *Fault status register
+ ******************************************************************************/
+
+#define MSDI_POR_STAT_MASK                    (0x00000080U)/*Power on reset mask*/
+#define MSDI_SPI_FAIL_MASK                    (0x00000040U)/*SPI Comm fail*/
+#define MSDI_PRTY_FAIL_MASK                   (0x00000020U)/*Parity fail mask*/
+#define MSDI_SSC_MASK                         (0x00000010U)/*Switch state change*/
+#define MSDI_RES_MASK                         (0x00000008U)/*Reserved and always at 0*/
+#define MSDI_TEMP_MASK                        (0x00000004U)/*Temperature event mask*/
+#define MSDI_OI_MASK                          (0x00000002U)/*Other interrupt*/
+
 
 //*****************************************************************************
 //
@@ -61,7 +94,19 @@ extern "C"
 //*****************************************************************************
 
 
+/*******************************************************************************
+ * TIVA Specific Functions
+ ******************************************************************************/
 
+
+
+/*TIVA specific SPI transfer function
+**Parameters:
+**send_data: The register, data, and parity bit to be sent.
+**rcv_data: Pointer to data to be stored before it is parsed
+ */
+extern msdi_status_t MSDI_DATA_TRANSFER(
+		const uint32_t send_data[NUM_SSI_DATA], uint32_t* const rcv_data[NUM_SSI_DATA]);
 
 
 //*****************************************************************************
@@ -73,4 +118,4 @@ extern "C"
 }
 #endif
 
-#endif // __MSDI_H__
+#endif /* __MSDI_H__ */
